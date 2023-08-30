5a143d36d076c097ba792767d4963ad9df672b09
Saeed Amiri <samiri@uni-bremen.de>
2023-08-30 14:18:05 +0200

    initiate the scripts

---
+++ codes/make_structure.py
@@ -0,0 +1,9 @@
+"""
+The scripts obtain the required residues and organize the input for
+Packmol.
+To achieve this, the input must include the dimensions of the overall
+box containing both nanoparticles and the source files for each
+residue. The center point between the two nanoparticles will be set at
+coordinates (0 0 0).
+"""
+
